102ae9ce1b4cff9059e72c2dcc1c32effceee2ff
C'tri <CtriCoding@Ctri.co.uk>
2018-09-21 16:06:20 +0100

    Looping of table

--- NS JavaScript.js
+++ NS JavaScript.js
@@ -1,10 +1,31 @@
-//var script = document.createElement('script'); script.src = "C:\Users\PeterGoudie\Desktop\NS JavaScript.js"; script.onload = function() {alert("Test");}; document.getElementsByTagName('head')[0].appendChild(script);
+//var script = document.createElement('script'); script.src = "https://github.com/Ctri-The-Third/NS-Helper/blob/master/NS%20JavaScript.js"; script.onload = function() {alert("Test");}; document.getElementsByTagName('head')[0].appendChild(script);
 
 function testFunction()
 {
 	alert("Loaded");
-	//$(".uir-pagination-select-wrapper")
-	//NS.UI.Helpers.uir_paginationSelectHelper.onPaginationNext(NS.jQuery(document.getElementsByClassName("uir-pagination-select-wrapper"))); event.stopPropagation(); return false;
+	
+	//loop
+	var targetTable = document.getElementById("neg706__tab");
+	
+	var targetTableRowsCount = targetTable.rows.length;
+	var targetTableColsCount = targetTable.rows[1].cells.length;
+	var output = new Array();
+		
+	var outputtext = ""; 
+	for (x = 1; x<=targetTableRowsCount; x++) 
+	{
+		var outputColumn = new Array();
+		for ( y= 0; y < targetTableColsCount; y++)
+		{
+			outputtext += targetTable.rows[x].cells[y].innerText + ","
+			outputColumn.push(targetTable.rows[x].cells[y].innerText);
+		}
+		outputtext += "\n";
+		output.push(outputColumn);
+
+	}
+
+	//NS.UI.Helpers.uir_paginationSelectHelper.onPaginationNext(NS.jQuery(document.getElementsByClassName("uir-pagination-select-wrapper"))); 
 }
 
 testFunction();
