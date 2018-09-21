93c9046bb5566ff6d97fee315f90c3e567f9c33b
C'tri <CtriCoding@Ctri.co.uk>
2018-09-21 15:40:17 +0100

    Initial commit

---
+++ NS JavaScript.js
@@ -0,0 +1,10 @@
+//var script = document.createElement('script'); script.src = "C:\Users\PeterGoudie\Desktop\NS JavaScript.js"; script.onload = function() {alert("Test");}; document.getElementsByTagName('head')[0].appendChild(script);
+
+function testFunction()
+{
+	alert("Loaded");
+	//$(".uir-pagination-select-wrapper")
+	//NS.UI.Helpers.uir_paginationSelectHelper.onPaginationNext(NS.jQuery(document.getElementsByClassName("uir-pagination-select-wrapper"))); event.stopPropagation(); return false;
+}
+
+testFunction();
